9ebbb0e1fae51e5beece4cf08a5eccb77246dfc0
tolulope23-ops <adeyemitoluadedoyin@gmail.com>
2025-02-22 09:52:43 +0100

    server setup

---
+++ app.js
@@ -0,0 +1,14 @@
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+
+const app = express(); 
+
+
+app.use(express.json());
+app.use(cors());
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () =>{
+    console.log("Server is listening...");
+})
