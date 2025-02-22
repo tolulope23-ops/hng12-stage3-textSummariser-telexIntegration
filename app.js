899afdbbfd27c81f6080ea8f03819dc233c0b42a
tolulope23-ops <adeyemitoluadedoyin@gmail.com>
2025-02-22 10:01:08 +0100

    feat: post request endpoint

--- app.js
+++ app.js
@@ -3,10 +3,37 @@ const express = require('express');
 const cors = require('cors');
 
 const app = express(); 
-
+const summariser = require('./utils/messageSummarizer');
 
 app.use(express.json());
 app.use(cors());
+
+app.post("/summarise", (req, res) => {
+    try {
+        let {settings, message } = req.body;
+        let sentenceLength;
+
+        // Extract settings
+        settings.forEach(setting => {
+            if (setting.label === "SentenceLength" && typeof setting.default === "number") {
+                sentenceLength = setting.default;
+            }
+        });
+
+        const summarisedMessage = summariser(message, sentenceLength)
+        res.json({
+            event_name: "message_formatted",
+            message: summarisedMessage,
+            status: "success",
+            username: "Rachael"
+        });
+    } catch (error) {
+        console.error("Error summarising message:", error);
+        res.status(500).json({ detail: "Internal Server Error" });
+    }
+});
+
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () =>{
